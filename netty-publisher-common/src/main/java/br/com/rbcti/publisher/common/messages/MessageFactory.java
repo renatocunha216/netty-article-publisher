daf5a16b1bb7546a4c4287defd831ddde3ccbbb2
rbcunha79 <rbcunha@yahoo.com>
2023-10-26 15:25:53 -0300

    -

--- netty-publisher-common/src/main/java/br/com/rbcti/publisher/common/messages/MessageFactory.java
+++ netty-publisher-common/src/main/java/br/com/rbcti/publisher/common/messages/MessageFactory.java
@@ -19,21 +19,21 @@ import br.com.rbcti.publisher.common.messages.fbs.ProtocolMessageFbs;
 public class MessageFactory {
 
     private MessageFactory() {
-        
+
     }
 
     public static SimpleMessage getMessageInstance(ByteBuffer flatBufferData) {
-        
+
         SimpleMessage message = null;
-        
+
         ProtocolMessageFbs messageProtocol = ProtocolMessageFbs.getRootAsProtocolMessageFbs(flatBufferData);
 
         switch (messageProtocol.messageType()) {
-        
+
             case AllProtocolMessagesFbs.loginRequestFbs:
                 message = decodeLoginRequest(messageProtocol);
                 break;
-    
+
             case AllProtocolMessagesFbs.loginResponseFbs:
                 message = decodeLoginResponse(messageProtocol);
                 break;
@@ -53,76 +53,76 @@ public class MessageFactory {
         return message;
     }
 
-    public static SimpleMessage getMessageInstance(byte [] flatBufferData) {
+    public static SimpleMessage getMessageInstance(byte[] flatBufferData) {
         return getMessageInstance(ByteBuffer.wrap(flatBufferData));
     }
-    
-    private static LoginRequest decodeLoginRequest(ProtocolMessageFbs _messageProtocol) {        
+
+    private static LoginRequest decodeLoginRequest(ProtocolMessageFbs _messageProtocol) {
         LoginRequestFbs login = (LoginRequestFbs) _messageProtocol.message(new LoginRequestFbs());
         HeaderFbs header = login.header();
-        
+
         LoginRequest loginRequest = new LoginRequest();
         loginRequest.setMessageId(header.messageId());
         loginRequest.setVersion(header.version());
         loginRequest.setUsn(header.usn());
         loginRequest.setUserName(login.userName());
         loginRequest.setPassword(login.password());
-        
-        return loginRequest;        
+
+        return loginRequest;
     }
-    
-    private static LoginResponse decodeLoginResponse(ProtocolMessageFbs _messageProtocol) {        
+
+    private static LoginResponse decodeLoginResponse(ProtocolMessageFbs _messageProtocol) {
         LoginResponseFbs response = (LoginResponseFbs) _messageProtocol.message(new LoginResponseFbs());
         HeaderFbs header = response.header();
-        
+
         LoginResponse loginResponse = new LoginResponse();
         loginResponse.setMessageId(header.messageId());
         loginResponse.setVersion(header.version());
         loginResponse.setUsn(header.usn());
         loginResponse.setAuthenticationToken(response.authenticationToken());
         loginResponse.setResponseCode(response.responseCode());
-        
-        return loginResponse;        
-    }    
-    
+
+        return loginResponse;
+    }
+
     private static PostArticleRequest decodePostArticleRequest(ProtocolMessageFbs _messageProtocol) {
-        
+
         PostArticleRequestFbs postArticle = (PostArticleRequestFbs) _messageProtocol.message(new PostArticleRequestFbs());
         HeaderFbs header = postArticle.header();
-        
+
         PostArticleRequest postArticleRequest = new PostArticleRequest();
         postArticleRequest.setMessageId(header.messageId());
         postArticleRequest.setVersion(header.version());
         postArticleRequest.setUsn(header.usn());
-        
+
         postArticleRequest.setAuthenticationToken(postArticle.authenticationToken());
         postArticleRequest.setSubject(postArticle.subject());
         postArticleRequest.setArticle(postArticle.article());
-        
-        final int tagsLength = postArticle.tagsLength(); 
-        
+
+        final int tagsLength = postArticle.tagsLength();
+
         if (tagsLength > 0) {
             for (int c = 0; c < tagsLength; c++) {
-                postArticleRequest.addTags(postArticle.tags(c));                                
+                postArticleRequest.addTags(postArticle.tags(c));
             }
         }
-        
-        return postArticleRequest;        
+
+        return postArticleRequest;
     }
-    
+
     private static PostArticleResponse decodePostArticleResponse(ProtocolMessageFbs _messageProtocol) {
-        
+
         PostArticleResponseFbs response = (PostArticleResponseFbs) _messageProtocol.message(new PostArticleResponseFbs());
         HeaderFbs header = response.header();
-        
+
         PostArticleResponse postArticleResponse = new PostArticleResponse();
         postArticleResponse.setMessageId(header.messageId());
         postArticleResponse.setVersion(header.version());
         postArticleResponse.setUsn(header.usn());
         postArticleResponse.setArticleUuid(response.articleUuid());
         postArticleResponse.setResponseCode(response.responseCode());
-        
-        return postArticleResponse;        
-    }      
+
+        return postArticleResponse;
+    }
 
 }
